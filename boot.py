030648116c95f5d08e45797c889cf166c8749972
ms <ms@nix.de>
2023-11-07 06:14:58 +0100

    check version after reboot

--- boot.py
+++ boot.py
@@ -28,9 +28,11 @@ else:
         f.close()
         os.remove('update.dat')
         if upf is "run update":
-            print("Running update now...")
-            ugit.pull_all(isconnected=True,reboot=False)
-            blink_obled(led, 0.1, 0.1, 0)
+            chk = ugit.check_update_version()
+            if chk is True:
+                print("Running update now...")
+                ugit.pull_all(isconnected=True,reboot=False)
+                blink_obled(led, 0.1, 0.1, 0)
         else:
             pass      
     except OSError:  # open failed -> normal boot
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from debug import dbgprint
 from servo import SERVOModule, servoaction
 from led import init_led, set_blink, init_obled, blink_obled
 from wificon import scan4ap, connectnsave
-from ugit import check_update_version
+
     
 
 # pinout 
@@ -133,19 +133,12 @@ async def system(request, ws):
             await ws.send(ujson.dumps({"wif":ans}))
         elif "ota" in ujdata:
             if ujdata["ota"] == "update":
-                chk = check_update_version()
-                gc.collect()
-                if chk is True:
-                    f = open("update.dat", "w")
-                    f.write("run update")
-                    f.close()
-                    await ws.send(ujson.dumps({"ota":"new version"}))
-                    blink_obled(led, 0.1, 0.2, 0)
-                    #machine.reset()
-                elif chk is False:
-                    await ws.send(ujson.dumps({"ota":"ok"}))
-                elif chk is None:
-                    await ws.send(ujson.dumps({"ota":"undefined"}))
+                f = open("update.dat", "w")
+                f.write("run update")
+                f.close()
+                await ws.send(ujson.dumps({"ota":"new version"}))
+                blink_obled(led, 0.1, 0.2, 10)
+                machine.reset()
             else:
                 await ws.send(ujson.dumps({"ota":"err"}))
         else:
